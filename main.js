c312de2c4c32c3917dbb5701af1f1625ccf6e1a3
Mathew <Moses.Mathew@finastra.com>
2023-08-22 14:51:33 +0530

    commit -O

--- main.js
+++ main.js
@@ -2,23 +2,17 @@ import "./style.css";
 
 const hmabergerElement = document.querySelector("[data-hamberger]");
 hmabergerElement.addEventListener("click", hambergerClick);
-const dropDownMenuElement = document.querySelector("[data-dropdown-menu]").classList;
+const dropDownMenuElement = document.querySelector(
+  "[data-dropdown-menu]"
+).classList;
 function hambergerClick() {
-    dropDownMenuElement.toggle('open');
+  dropDownMenuElement.toggle("open");
 }
 
-
-const gridButton_Two_Element=document.querySelector("[data-two]")
-gridButton_Two_Element.addEventListener("click",gridButton_Two_Click)
-function gridButton_Two_Click() {
-    gridButton_Two_Element.classList.toggle("grid_boder")
-}
-
-
-const gridButton_list=document.querySelectorAll(".grid-item");
+const gridButton_list = document.querySelectorAll(".grid-item");
 gridButton_list.forEach((item) => {
-    console.log(item.dataset.one )
-    gridButton_Element=
-    item.addEventListener("click",() => {})
-})
-
+  console.log(item);
+  const gridButton_Element = item.addEventListener("click", () => {
+    item.classList.toggle("grid_boder");
+  });
+});
